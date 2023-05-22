ed9f60bbe557dd64f9249471d9f4847fddbc014b
mrumbra361 <122780690+mrumbra361@users.noreply.github.com>
2023-05-22 10:07:43 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == "adelante") {
+        cuteBot.motors(60, 60)
+    }
+    if (receivedNumber == "derecha") {
+        cuteBot.motors(10, 40)
+    }
+    if (receivedNumber == "izquierda") {
+        cuteBot.motors(40, 10)
+    }
+})
 basic.forever(function () {
-	
+    radio.setGroup(208)
 })
